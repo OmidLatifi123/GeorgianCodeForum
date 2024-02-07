6a7f9c2df6366fa99277a37655073cb1b3f3e357
Omid Latifi <99961178+OmidLatifi123@users.noreply.github.com>
2024-02-06 23:45:57 -0500

    Major Home page revisions

--- JS/home.js
+++ JS/home.js
@@ -4,5 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
         var splashScreen = document.getElementById("splash-screen");
         splashScreen.classList.add("fade-out"); // Add the fade-out class
         document.body.style.overflow = "auto"; // Re-enable scrolling after the fade-out
+        // Hide the logo after the fade-out transition completes
+        setTimeout(function () {
+            var logo = document.getElementById("logo");
+            logo.style.display = "none";
+        }, 2000);
     }, 2000);
 });
